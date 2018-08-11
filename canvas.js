60328cae8c9572f96a4bbbea4bca2bc442c2ef14
Seth Kujawa <sethkujawa@gmail.com>
2018-08-11 18:20:54 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,3 +1,14 @@
+
+// Check if Mobile // 
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+// Check if Mobile //
+let mobile = mobilecheck();
+
+
 // Canvas Setup //
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
@@ -468,6 +479,33 @@ class SettingFour {
 
 //End Setting Four Classes //
 
+// Setting Five Classes //
+class SettingFive {
+
+	constructor(){
+		
+	}
+
+	draw(){
+		
+	}
+
+	endSetting() {
+		
+	}
+
+	beginSetting() {
+		this.stopper = false;
+		$("#item").fadeOut("slow", function(){
+			$("#item").replaceWith($("<div id='item'><img src='assets/smoke.png' height='20px' width='20px'></div> <!-- End of Umbrella Div -->"));
+			$("#item").fadeOut(0);
+			$("#item").fadeIn("slow");
+		});
+	}
+};
+
+//End Setting Five Classes //
+
 // Regulate the Seasons //
 settings = [];
 settings.push(new SettingZero());
@@ -475,6 +513,7 @@ settings.push(new SettingOne());
 settings.push(new SettingTwo());
 settings.push(new SettingThree());
 settings.push(new SettingFour());
+settings.push(new SettingFive());
 nextSetting = 0;
 changeSettings();
 
@@ -488,6 +527,7 @@ function changeSettings(){
 		settings[2].endSetting()
 		settings[3].endSetting()
 		settings[4].endSetting()
+		settings[5].endSetting()
 		colorChange = true;
   		fr = 25;
   		fg = 25;
@@ -503,6 +543,7 @@ function changeSettings(){
 		settings[2].endSetting()
 		settings[3].endSetting()
 		settings[4].endSetting()
+		settings[5].endSetting()
 		colorChange = true;
 		fr = 135;
   		fg = 206;
@@ -519,6 +560,7 @@ function changeSettings(){
 		settings[2].beginSetting()
 		settings[3].endSetting()
 		settings[4].endSetting()
+		settings[5].endSetting()
 		colorChange = true;
   		fr = 120;
   		fg = 200;
@@ -535,6 +577,7 @@ function changeSettings(){
 		settings[2].endSetting()
 		settings[3].beginSetting()
 		settings[4].endSetting()
+		settings[5].endSetting()
 		colorChange = true;
 		fr = 100;
   		fg = 100;
@@ -545,12 +588,30 @@ function changeSettings(){
   		sb = 200;
 	}
 	else if (nextSetting == 4){
-		nextSetting = 0;
+		nextSetting = 5;
 		settings[0].endSetting()
 		settings[1].endSetting()
 		settings[2].endSetting()
 		settings[3].endSetting()
 		settings[4].beginSetting()
+		settings[5].endSetting()
+		colorChange = true;
+		fr = 70;
+  		fg = 70;
+  		fb = 120;
+
+  		sr = 170;
+  		sg = 150;
+  		sb = 180;
+	}
+	else if (nextSetting == 5){
+		nextSetting = 0;
+		settings[0].endSetting()
+		settings[1].endSetting()
+		settings[2].endSetting()
+		settings[3].endSetting()
+		settings[4].endSetting()
+		settings[5].beginSetting()
 		colorChange = true;
 		fr = 70;
   		fg = 70;
@@ -570,7 +631,7 @@ randomSettingChanger = true;
 function animate() {
 	if (randomSettingChanger == true){
 		timer ++;
-		if (timer == 1000){
+		if (timer == 500){
 			timer = 0;
 			changeSettings();
 		}
@@ -630,7 +691,12 @@ function animate() {
 			bool = false;
 		}
 	}
-	document.getElementById("canvas").style.background = "linear-gradient(rgba(" + fgr.toString() + "," + fgg.toString() + "," + fgb.toString() + "), rgb(" + sgr.toString() + "," + sgg.toString() + "," + sgb.toString() + ", 0.95))"
+	if (mobile == true){
+		document.getElementById("canvas").style.background = "rgb(" + fgr.toString() + "," + fgg.toString() + "," + fgb.toString() + ")";
+	} else{
+		document.getElementById("canvas").style.background = "linear-gradient(rgba(" + fgr.toString() + "," + fgg.toString() + "," + fgb.toString() + "), rgb(" + sgr.toString() + "," + sgg.toString() + "," + sgb.toString() + ", 0.95))"
+	}
+	
 }
 
 animate();
@@ -832,13 +898,17 @@ document.addEventListener('keydown', (event) => {
   		$("#locInfo").text("Zip: " + zipCode.toString() + "  |  " + currentWeather.toString()); 
   		nextSetting = 2;
 		changeSettings();
-	} else if (currentWeather == "Cloudy"){
+	} else if (currentWeather == "Clouds"){
   		$("#locInfo").text("Zip: " + zipCode.toString() + "  |  " + currentWeather.toString()); 
   		nextSetting = 3;
 		changeSettings();
 	} else if (currentWeather == "Thunderstorm"){
   		$("#locInfo").text("Zip: " + zipCode.toString() + "  |  " + currentWeather.toString()); 
   		nextSetting = 4;
+		changeSettings();
+	} else if (currentWeather == "Smoke"){
+  		$("#locInfo").text("Zip: " + zipCode.toString() + "  |  " + currentWeather.toString()); 
+  		nextSetting = 5;
 		changeSettings();
   	} else {
   		$("#locInfo").text("Zip: " + zipCode.toString() + "  |  " + currentWeather.toString()); 
@@ -848,3 +918,9 @@ document.addEventListener('keydown', (event) => {
 });
 
 // End Event Handlers //
+
+
+
+
+
+
